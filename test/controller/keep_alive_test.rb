95e2b74570671180c9011ed1d1ac3ee1eb0714d9
Pratik Naik <pratiknaik@gmail.com>
2009-12-25 14:10:50 +0530

    Add keep alive tests

---
+++ test/controller/keep_alive_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class KeepAliveTest < Cramp::Controller::TestCase
+
+  class KeepAliveController < Cramp::Controller::Base
+    keep_connection_alive :every => 0
+  end
+
+  def app
+    KeepAliveController
+  end
+
+  def test_keep_alive
+    get_body_chunks '/', :count => 2 do |chunks|
+      assert " ", chunks[0]
+      assert " ", chunks[1]
+    end
+  end
+
+end
