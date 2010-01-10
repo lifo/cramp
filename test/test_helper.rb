8e19afab4bc10a680a3aa7764995291a31685207
Pratik Naik <pratiknaik@gmail.com>
2010-01-11 02:22:11 +0530

    Dont require cramp/model from the tests at the moment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,6 @@ cramp_path = File.join(File.dirname(__FILE__), "../lib")
 $:.unshift(cramp_path) unless $:.include?(cramp_path)
 
 require 'cramp/controller'
-require 'cramp/model'
 
 require 'test/unit'
 require 'usher'
