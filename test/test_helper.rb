2eeefa3156f0af3cfecd05a052701a168dca227f
Pratik Naik <pratiknaik@gmail.com>
2010-04-21 15:37:49 +0100

    Add a logger to tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,8 @@ Bundler.require :default, :test
 require 'cramp/controller'
 require 'test/unit'
 require 'usher'
+
+require 'active_support/buffered_logger'
+logger = ActiveSupport::BufferedLogger.new(File.join(File.dirname(__FILE__), "tests.log"))
+logger.level = ActiveSupport::BufferedLogger::DEBUG
+Cramp.logger = logger
