2f8ba35cf994f06c0ea41b8209c72da97da1362e
Pratik Naik <pratiknaik@gmail.com>
2011-08-01 22:49:20 +0100

    Sigh. Whitespaces

--- lib/cramp/generators/templates/application/app/actions/home_action.rb
+++ lib/cramp/generators/templates/application/app/actions/home_action.rb
@@ -1,10 +1,10 @@
 class HomeAction < Cramp::Action
-  <% if active_record? %>use_fiber_pool do |pool|
+  <% if active_record? -%>use_fiber_pool do |pool|
     # Checkin database connection after each callback
     pool.generic_callbacks << proc { ActiveRecord::Base.clear_active_connections! }
-  end<% end %>
+  end
 
-  def start
+  <% end %>def start
     render "Hello World!"
     finish
   end
